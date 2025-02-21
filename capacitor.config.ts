873438f56ba75e94d6524393ff9b7216ad8c6639
Isaias <isasalas145@gmail.com>
2025-02-21 16:46:53 -0400

    first commit

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,9 @@
-import type { CapacitorConfig } from '@capacitor/cli';
+import type { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
-  appId: 'com.example.app',
-  appName: 'front',
-  webDir: 'public'
+  appId: "com.example.app",
+  appName: "front",
+  webDir: "public",
 };
 
 export default config;
